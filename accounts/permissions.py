ec90bf08d628f92cc456c2f7d6e01ec64aa4fc12
rohn21 <rohn2100@gmail.com>
2025-03-05 11:09:22 +0530

    formatted code

--- accounts/permissions.py
+++ accounts/permissions.py
@@ -1,5 +1,15 @@
 from rest_framework.permissions import BasePermission, SAFE_METHODS
 
+
+class IsAdminOrReadonly(BasePermission):
+    message = 'You must be the owner of this object.'
+
+    def has_permission(self, request, view):
+        if request.user and request.user.is_staff:
+            return True
+        return request.method in SAFE_METHODS
+
+
 class IsOwnerOrReadonly(BasePermission):
     message = 'You must be the owner of this object.'
 
@@ -7,7 +17,8 @@ class IsOwnerOrReadonly(BasePermission):
         if request.user and request.user.is_authenticated:
             return True
         return request.method in SAFE_METHODS
-    # def has_object_permission(self, request, view, obj):
-    #     if obj.user == request.user: # review  obj.user
-    #         return True
-    #     return request.method in SAFE_METHODS
+
+    def has_object_permission(self, request, view, obj):
+        if obj.user == request.user:  # review  obj.user
+            return True
+        return request.method in SAFE_METHODS
--- accounts/serializers.py
+++ accounts/serializers.py
@@ -4,6 +4,7 @@ from django.contrib.auth import get_user_model
 
 User = get_user_model()
 
+
 class CustomUserSerializer(serializers.ModelSerializer):
     confirm_password = serializers.CharField(style={'input_type': 'password'}, source='password2', write_only=True)
 
@@ -46,89 +47,8 @@ class CustomUserSerializer(serializers.ModelSerializer):
         instance.save()
         return instance
 
-# class UserSerializer(serializers.ModelSerializer):
-#     profiles = UserProfileDetailSerializer()
-#
-#     class Meta:
-#         model = User
-#         fields = ['id', 'email', 'username', 'mobile_number', 'profiles']
-#
-#     def update(self, instance, validated_data):
-#         profile_data = validated_data.pop('profiles', {})
-#         instance = super().update(instance, validated_data)
-#
-#         profile = instance.profiles
-#         profile.profile_pic = profile_data.get('profile_pic', profile.profile_pic)
-#         profile.save()
-#
-#         return instance
-
-    # def update(self, instance, validated_data):
-    #     # Update the user fields
-    #     profile_data = validated_data.pop('profiles', None)
-    #     for attr, value in validated_data.items():
-    #         setattr(instance, attr, value)
-    #
-    #     instance.save()
-    #
-    #     if profile_data:
-    #         profile = instance.profile
-    #         for attr, value in profile_data.items():
-    #             setattr(profile, attr, value)
-    #         profile.save()
-
-        # return instance
-#
-# class UserUpdateSerializer(serializers.ModelSerializer):
-#     email = serializers.EmailField(required=False)
-#     username = serializers.CharField(required=False)
-#     mobile_number = serializers.CharField(required=False)
-#
-#     class Meta:
-#         model = User
-#         fields = ['email', 'username', 'mobile_number']
-#
-#     def update(self, instance, validated_data):
-#         for attr, value in validated_data.items():
-#             setattr(instance, attr, value)
-#         instance.save()
-#         return instance
-#
-# class ProfileUpdateSerializer(serializers.ModelSerializer):
-#     profile_pic = serializers.ImageField(required=False, allow_null=True)
-#
-#     class Meta:
-#         model = Profile
-#         fields = ['profile_pic']
-#
-#     def update(self, instance, validated_data):
-#         for attr, value in validated_data.items():
-#             setattr(instance, attr, value)
-#         instance.save()
-#         return instance
-#
-# class UserProfileUpdateSerializer(serializers.Serializer):
-#     user = UserUpdateSerializer(required=False)
-#     profile = ProfileUpdateSerializer(required=False)
-#
-#     def update(self, instance, validated_data):
-#         user_data = validated_data.pop('user', None)
-#         profile_data = validated_data.pop('profile', None)
-#
-#         if user_data:
-#             user_serializer = UserUpdateSerializer(instance.user, data=user_data, partial=True)
-#             if user_serializer.is_valid():
-#                 instance.user = user_serializer.save()
-#
-#         if profile_data:
-#             profile_serializer = ProfileUpdateSerializer(instance.profile, data=profile_data, partial=True)
-#             if profile_serializer.is_valid():
-#                 instance.profile = profile_serializer.save()
-#
-#         return instance
 
 class UserSerializer(serializers.ModelSerializer):
-
     class Meta:
         model = User
         fields = ['id', 'email', 'username', 'mobile_number']
@@ -136,6 +56,18 @@ class UserSerializer(serializers.ModelSerializer):
             'email': {'read_only': True},
             'mobile_number': {'required': False},
         }
+
+
+class UserLoginSerializer(serializers.ModelSerializer):
+    email = serializers.ReadOnlyField(source="user.email")
+    mobile_number = serializers.ReadOnlyField(source="user.mobile_number")
+    is_staff = serializers.ReadOnlyField(source="user.is_staff")
+    is_active = serializers.ReadOnlyField(source="user.is_active")
+
+    class Meta:
+        model = User
+        fields = ['id', 'email', 'username', 'mobile_number', 'is_staff', 'is_active']
+
 
 class UserDetailsSerializer(serializers.ModelSerializer):
     user = UserSerializer(required=False)
